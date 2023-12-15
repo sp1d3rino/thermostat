4554e0d0215547ef9aa513e65c6b60bc8fb652f1
Fabrizio-Rastiello <fabrizio.rastiello@kyndryl.com>
2023-12-15 22:28:15 +0100

    test

--- server.py
+++ server.py
@@ -11,3 +11,4 @@ Handler.extensions_map[".html"] = "text/html"
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("Server started at localhost:" + str(PORT))
     httpd.serve_forever()
+    
