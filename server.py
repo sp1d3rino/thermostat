4e782c2d6d5b3aba7e85d6bbc9ad2e8813053e8f
sp1d3rino <fabrizio.rastiello@kyndryl.com>
2023-12-16 01:21:00 +0100

    read

--- server.py
+++ server.py
@@ -5,6 +5,11 @@ import datetime
 
 api = Flask(__name__)
 
+def write_status(value):
+  f = open("tstatus.txt", "w")
+  f.write(value)
+  f.close()
+
 def log(value):
    # Append-adds at last
   file1 = open("history", "a")  # append mode
@@ -15,17 +20,19 @@ def log(value):
 
 @api.route('/', methods=['GET'])
 def get_index():
-  status= os.getenv("TSTATUS")
+  f = open("tstatus.txt", "r")
+  status= f.read()
   return render_template('index.html', pos=status)
 
 @api.route('/on', methods=['GET'])
 def get_on():
-  os.environ['TSTATUS']="130"
+  write_status("130")
   log("130")
   return render_template('index.html', pos="130")
 
 @api.route('/off', methods=['GET'])
 def get_off():
+  write_status("90")
   log("90")
   os.environ['TSTATUS']="90"
   return render_template('index.html', pos="90")
