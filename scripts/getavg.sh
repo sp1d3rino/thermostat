f88ee9514c03445e11fb9d8d6c25eee029ec29b7
sp1d3rino <fabrizio.rastiello@kyndryl.com>
2023-12-24 17:12:43 +0100

    fix avg

--- scripts/getavg.sh
+++ scripts/getavg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-count=0; total=0; for i in $( awk '{ print $4; }' ../temp-history );\
+count=0; total=0; for i in $( awk '{ print $4; }' temp-history );\
 do total=$(echo $total+$i | bc ); \
 ((count++)); done; echo "scale=2; $total / $count" | bc
