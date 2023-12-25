7c6e34a4960050e1427004eb268d3477d3fcce1b
sp1d3rino <fabrizio.rastiello@kyndryl.com>
2023-12-25 01:08:20 +0100

    tempavg

--- scripts/getavg.sh
+++ scripts/getavg.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-count=0; total=0; for i in $( awk '{ print $4; }' temp-history );\
-do total=$(echo $total+$i | bc ); \
-((count++)); done; echo "scale=2; $total / $count" | bc
+awk '{ print $3; }' tempavg
